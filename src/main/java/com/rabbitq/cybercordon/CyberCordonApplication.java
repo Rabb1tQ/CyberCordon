fe9772e78d226ca39d196d6d0e48badcc03254e6
Rabb1tQ <15024835320@163.com>
2024-06-04 22:51:02 +0800

    初始化项目，集成mybatis及spring-doc

---
+++ src/main/java/com/rabbitq/cybercordon/CyberCordonApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitq.cybercordon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberCordonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberCordonApplication.class, args);
+    }
+
+}
